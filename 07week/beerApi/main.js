984b46f1a1b7b2833f28e266174f31b232b5a6eb
devarcher <archershot@hotmail.com>
2019-10-03 14:31:57 -0500

    first fetch push

--- 07week/beerApi/main.js
+++ 07week/beerApi/main.js
@@ -8,18 +8,19 @@ let beers = [];
 // this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
 const getBeerLocations = () => {
   const proxy = "https://cors-anywhere.herokuapp.com/";
+  const api = `${proxy}http://api.brewerydb.com/v2/beers/?key=e65f7926d6ffd51706e8231e43b7cf78`;
 
-  // const api = ;
   fetch(api)
     .then(res => res.json())
     .then(data => {
       beers = data;
-      console.log(beers);
+      displayBeers();
     });
 };
 
-// displayBeers = () => {
-//   console.log(beers);
-// };
+const displayBeers = () => {
+  console.log(beers);
+  
+};
 
-// displayBeers();
+

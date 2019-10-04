c31875d0f50d8ee0e373920f694d42695029687c
devarcher <archershot@hotmail.com>
2019-10-04 11:27:58 -0500

    converted beers to an array, untested filter written for irish beers. still experiencing 429 error due to size of data requests.

--- 07week/beerApi/main.js
+++ 07week/beerApi/main.js
@@ -9,21 +9,21 @@ let beers = [];
 // this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
 const getBeerLocations = () => {
   const proxy = "https://cors-anywhere.herokuapp.com/";
-  const apiCategory = `${proxy}http://api.brewerydb.com/v2/categories/?key=e65f7926d6ffd51706e8231e43b7cf78`;
+  // const apiCategory = `${proxy}http://api.brewerydb.com/v2/categories/?key=e65f7926d6ffd51706e8231e43b7cf78`;
   const apiBeers = `${proxy}http://api.brewerydb.com/v2/beers/?key=e65f7926d6ffd51706e8231e43b7cf78`;
 
-  fetch(apiCategory)
-    .then(res => res.json())
-    .then(data => {
-      categories = data;
-      displayCategories();
-    });
+  // fetch(apiCategory)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     categories = data;
+  //     displayCategories();
+  //   });
 
   fetch(apiBeers)
     .then(res => res.json())
     .then(data => {
-      beers = data.filter(id => id === 2);
-      console.log(beers)
+      beers = data
+      displayBeers();
     });
 };
 
@@ -31,7 +31,14 @@ const displayCategories = () => {
   console.log(categories);
 };
 
-const displayBeers = (irishBeers) => {
-  console.log(beers);
+const displayBeers = () => {
+  const beerArr = []
+  beerArr.push(beers)
+
+  const irishBeers = beerArr.filter(beer => beer.id === 2);
+
+  
+  console.log(Object.prototype.toString.call(beers));
+  console.log(Object.prototype.toString.call(beerArr));
   console.log(irishBeers);
 };

0c9825e5cb36253840d47acab93200b6967c9ba8
devarcher <archershot@hotmail.com>
2019-10-03 23:18:54 -0500

    boom, crash, blam. need to reduce amount of data i'm pulling. getting 429, too many requests

--- 07week/beerApi/main.js
+++ 07week/beerApi/main.js
@@ -3,24 +3,35 @@ window.onload = function() {
   getBeerLocations();
 };
 
+let categories = [];
 let beers = [];
 
 // this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
 const getBeerLocations = () => {
   const proxy = "https://cors-anywhere.herokuapp.com/";
-  const api = `${proxy}http://api.brewerydb.com/v2/beers/?key=e65f7926d6ffd51706e8231e43b7cf78`;
+  const apiCategory = `${proxy}http://api.brewerydb.com/v2/categories/?key=e65f7926d6ffd51706e8231e43b7cf78`;
+  const apiBeers = `${proxy}http://api.brewerydb.com/v2/beers/?key=e65f7926d6ffd51706e8231e43b7cf78`;
 
-  fetch(api)
+  fetch(apiCategory)
     .then(res => res.json())
     .then(data => {
-      beers = data;
-      displayBeers();
+      categories = data;
+      displayCategories();
+    });
+
+  fetch(apiBeers)
+    .then(res => res.json())
+    .then(data => {
+      beers = data.filter(id => id === 2);
+      console.log(beers)
     });
 };
 
-const displayBeers = () => {
-  console.log(beers);
-  
+const displayCategories = () => {
+  console.log(categories);
 };
 
-
+const displayBeers = (irishBeers) => {
+  console.log(beers);
+  console.log(irishBeers);
+};
